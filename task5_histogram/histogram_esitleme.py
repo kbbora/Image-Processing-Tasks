87f29d974cb555619cdfe622950a1f56a8277bfd
Bora Bostancı <kbborabostanci@gmail.com>
2024-07-12 11:02:38 +0300

    Update histogram_esitleme.py

--- task5_histogram/histogram_esitleme.py
+++ task5_histogram/histogram_esitleme.py
@@ -3,21 +3,16 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread(r"C:\Users\BORA\PythonProjeleri\PythonProjeleri\images\hist_equ.jpg", 0)
 
-
 equ = cv2.equalizeHist(img)
 
-plt.figure(figsize= (12,6))
+plt.figure(figsize=(12, 6))
 
-
-plt.subplot(1,2,1)
+plt.subplot(1, 2, 1)
 plt.imshow(img, cmap='gray')
 plt.title('Original')
 
-plt.subplot(1,2,2)
+plt.subplot(1, 2, 2)
 plt.imshow(equ, cmap='gray')
 plt.title('Equalized Image')
 
 plt.show()
-
-
-

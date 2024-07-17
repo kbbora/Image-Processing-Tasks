12133644c2c6f5ebdcd31f33cc3ff80710bc6043
Bora Bostancı <kbborabostanci@gmail.com>
2024-07-17 10:39:20 +0300

    Update task_kivi.py

--- task6_filtreleme/task_kivi.py
+++ task6_filtreleme/task_kivi.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread(r"C:\Users\BORA\PythonProjeleri\PythonProjeleri\images\kivi.jpeg")
+img = cv2.imread("kivi.jpeg")
 
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 

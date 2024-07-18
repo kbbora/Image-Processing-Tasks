1a38504cb500c980c59979e0a3e941878f77a89d
Bora Bostancı <kbborabostanci@gmail.com>
2024-07-18 16:47:10 +0300

    Add files via upload

---
+++ task7_Feature_Detection_And_Matching/ORB.py
@@ -0,0 +1,22 @@
+import cv2
+import matplotlib.pyplot as plt
+
+img1 = cv2.imread(r'C:\Users\BORA\PythonProjeleri\PythonProjeleri\images\kizkulesi.jpg')
+img2 = cv2.imread(r'C:\Users\BORA\PythonProjeleri\PythonProjeleri\images\kizkulesi2.jpg')
+
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+# ORB
+orb = cv2.ORB_create()
+
+keypoints_1, descriptors_1 = orb.detectAndCompute(img1, None)
+keypoints_2, descriptors_2 = orb.detectAndCompute(img2, None)
+
+# özellik eşleşmeleri
+bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+matches = bf.match(descriptors_1, descriptors_2)
+matches = sorted(matches, key=lambda x: x.distance)
+
+img3 = cv2.drawMatches(img1, keypoints_1, img2, keypoints_2, matches[:100], img2, flags=2)
+plt.imshow(img3), plt.show()
